c9c3ea1018ca29451e4c286e8ea7412d3a719c67
Galaxy111101000 <aneyakazmi@gmail.com>
2025-09-08 10:15:25 -0400

    prints number of multiples of 3 and/or 5

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args){
+        int count = 0;
+
+        for(int i = 0; i < 1000; i++){
+            boolean multipleOf5 = (i % 5 == 0);
+            boolean multipleOf3 = (i % 3 == 0);
+
+            if (multipleOf5 || multipleOf3) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+
+}
